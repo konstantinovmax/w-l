ae74ee2f1e67d37a5867540fe4d2e4a791199a80
Maxim <konstantinovmax@gmail.com>
2021-07-05 10:18:27 +0300

    fix: править базовую структуру проекта

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import logo from '../images/gear.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1 class="header__title">Сайт в разработке</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
